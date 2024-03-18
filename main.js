e24a7ac73eaf84babd3a5fdcef60d6a65a9194cc
Fidan Aghayeva <fidan.aghay@gmail.com>
2024-03-18 19:53:54 +0400

    z

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-console.log("salam")
-console.log("hello")
+console.clear();
+
+var(property) name: String
+name: "Fidan"
+surname:"lala"
+age: "18"
+"has-car" : false
+}
+
+
+user1.surname = "Aghayeva"
+user1.surname = "Agayeva"
+user1.age = 19
+
+console.log(user1);
